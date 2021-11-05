82fb440e6016f9310967ddec0a884d259b905255
ThePiranhaGuy <arunava.sarkar.ar@outook.com>
2021-11-05 17:59:03 +0530

    added misc

---
+++ main.py
@@ -0,0 +1,23 @@
+from knowledgebase import *
+from esframework import *
+
+def report_findings(findings):
+    for inst, result in findings.items():
+        print('Findings for %s-%d:' % (inst[0], inst[1]))
+        
+        for param, vals in result.items():
+            possibilities = ['%s: %f' % (val[0], val[1]) for val in vals.items()]
+            if len(possibilities)<1:
+                print("None applicable")
+                break
+            print('%s: %s' % (param, ', '.join(possibilities)))
+        
+def main():
+    sh = Shell()
+    define_contexts(sh)
+    define_params(sh)
+    define_rules(sh)
+    report_findings(sh.execute(['material']))
+    
+if __name__=="__main__":
+    main()
